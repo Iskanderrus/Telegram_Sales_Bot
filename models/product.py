f7094ee2418a31066b93216fc7577e2bc00a534d
Iskanderrus <alex_ru2002@list.ru>
2022-08-01 21:34:16 +0300

    Added product model

--- models/product.py
+++ models/product.py
@@ -3,3 +3,28 @@ from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 from models.category import Category
 
+Base = declarative_base()
+
+
+class Product(Base):
+    __tablename__ = 'products'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, index=True)
+    title = Column(String)
+    price = Column(Float)
+    quantity = Column(Integer)
+    is_active = Column(Boolean)
+    category_id = Column(Integer, ForeignKey('category.id'))
+
+    category = relationship(
+        Category,
+        backref=backref(
+            'products',
+            uselist=True,
+            cascade='delete, all'
+        )
+    )
+
+    def __str__(self):
+        return f'{self.name} {self.title} {self.price}'
